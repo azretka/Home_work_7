6b6f47a683839d7bb27b0be978a1620ed1d87019
azretka <azret.karagulov@gmail.com>
2023-05-19 22:43:23 +0600

    home_work_5

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
-var list = [null, 123, undefined, null, "qwerty", 34, 65, undefined, null, null, 43, "aerg", "пкцйк", null, `uioo`]
-var answer = []
-var numbers= []
-var nulls = []
-var undefineds = []
-var strings = []
+const books = ['Первый учитель', 'Гарри Поттер', 'Ведьмак', 'Шерлок Холмс'];
 
-for (var i of list){
-    if(i === Number(i)){
-        numbers.push(i)
-    }else if(i === String(i)){
-        strings.push(i)
-    }else if(i === null){
-        nulls.push(i)
-    }else if(i === undefined){
-        undefineds.push(i)
+function booksList () {
+    const uBooks = []
+    const restBooks = []
+
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].includes('м')) {
+            uBooks.push(books[i]);
+        } else {
+            restBooks.push(books[i])
+        }
     }
+    return {uBooks, restBooks};
 }
-answer.push(numbers)
-answer.push(nulls)
-answer.push(undefineds)
-answer.push(strings)
+const {uBooks, restBooks} = booksList(books)
 
-console.log(answer.sort((w, q) => q.length - w.length))
+console.log('Есть буква "м":', uBooks);
+console.log('Остальные книги:', restBooks);
+
+function printDelimiter() {
+    console.log('-'.repeat(100))
+}
+printDelimiter()
+
+function getAverage(...nums){
+    console.log(nums)
+    let sum = nums.reduce((a, b) => a+b, 0)
+    let average;
+    average = sum / nums.length;
+    return average;
+}
+console.log(getAverage(1, 10, 20, 30, 40, 50));
+console.log(getAverage(99, 255, 488));
